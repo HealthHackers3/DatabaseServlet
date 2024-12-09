d60d99cfac552d11b6d8cb5d81fb690000df7ea0
Sam Clifford <sc2522@ic.ac.uk>
2024-12-09 17:36:43 +0000

    newservletcommit V1.0

---
+++ src/main/java/Servlet.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.util.stream.Collectors;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.sql.*;
+@WebServlet(urlPatterns={"/patients","/doctors"},loadOnStartup = 1)
+public class Servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doGet");
+        resp.setContentType("text/html");
+        resp.getWriter().write(req.getServletPath());
+    }
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        String reqBody=req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+//        resp.setContentType("text/html");
+//        resp.getWriter().write("Thank you client! "+reqBody);
+
+    }
+
+}
