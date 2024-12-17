26aae5d2be245a54ec3a161d1752438d36a51cf0
Sam Clifford <sc2522@ic.ac.uk>
2024-12-17 13:01:45 +0000

    NS V1.9

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,29 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main  {
     public static void main(String[] args) throws SQLException {
-        System.out.println("Hello world!");
-        String dbUrl = "jdbc:postgresql://"+System.getenv("PGHOST")+":"+System.getenv("PGPORT")+"/"+System.getenv("PGDATABASE");
-        System.out.println(dbUrl);
-        DatabaseHandler dbh = new DatabaseHandler();
+        String dbUrl = "jdbc:postgresql://localhost:5432/postgres";
+        System.out.println("Connecting to " + dbUrl);
+        try {
+            // Registers the driver
+            Class.forName("org.postgresql.Driver");
+        } catch (Exception e) {
+        }
+        Connection conn= DriverManager.getConnection(dbUrl, "postgres", "guardspine");
+
+        try {
+            Statement s=conn.createStatement();
+            String sqlStr = "INSERT INTO patients (familyname,givenname,phonenumber) values('Swire22', 'Catherine', '07755678899')";
+            s.execute(sqlStr);
+
+            s.close();
+            conn.close();
+        }
+        catch (Exception e){
+        }
+
     }
 }
--- src/main/java/Servlet.java
+++ src/main/java/Servlet.java
@@ -1,7 +1,12 @@
 import com.google.gson.Gson;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +16,25 @@ import javax.servlet.http.HttpServletResponse;
 import java.sql.*;
 @WebServlet(urlPatterns={"/patients","/doctors"},loadOnStartup = 1)
 public class Servlet extends HttpServlet {
+    Connection conn;
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        //String dbUrl = "jdbc:postgresql://localhost:5432/postgres";
+        String dbUrl = "jdbc:postgresql://"+System.getenv("PGHOST")+":"+System.getenv("PGPORT")+"/"+System.getenv("PGDATABASE");
+        System.out.println("Connecting to " + dbUrl);
+        try {
+            // Registers the driver
+            Class.forName("org.postgresql.Driver");
+        } catch (Exception e) {
+        }
+        try {
+            //conn= DriverManager.getConnection(dbUrl, "postgres", "guardspine");
+            conn= DriverManager.getConnection(dbUrl,System.getenv("PGUSER"),System.getenv("PGPASSWORD"));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("doGet");
@@ -20,11 +44,48 @@ public class Servlet extends HttpServlet {
     }
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+            System.out.println("doPost");
             // Read the bytes of the body – ie the message
-            String reqBody=req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
-            resp.setContentType("text/html");
-            resp.getWriter().write("Thank you client! "+reqBody);
+//            String reqBody=req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+//            resp.setContentType("text/html");
+//            System.out.println(reqBody);
+//            resp.getWriter().write("Thank you client! "+reqBody);
+            //SQL EXECUTE
+        String sqlQuery=req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        resp.setContentType("application/json");
+
+        try (Statement s = conn.createStatement()){
+            boolean wantsResponse = s.execute(sqlQuery);
+            if(wantsResponse){
+                ResultSet rs = s.getResultSet();
+                ResultSetMetaData rsmd = rs.getMetaData();
+                int columnCount = rsmd.getColumnCount();
+                List<Map<String, Object>> resultList = new ArrayList<>();
+                while(rs.next()){
+                    Map<String, Object> row = new HashMap<>();
+                    for (int i = 1; i <= columnCount; i++) {
+                        String columnName = rsmd.getColumnName(i);
+                        Object columnValue = rs.getObject(i);
+                        row.put(columnName, columnValue);
+                    }
+                    resultList.add(row);
+                }
+                // Convert the result to JSON using Gson
+                Gson gson = new Gson();
+                String jsonResponse = gson.toJson(resultList);
+
+                // Send the JSON response back to the client
+                resp.getWriter().write(jsonResponse);
+            }
+            else{
+                int updateCount = s.getUpdateCount();
+                resp.getWriter().write("Update successful :) Rows affected: " + updateCount);
+            }
+
+        } catch (SQLException e) {
+            resp.getWriter().write("SQL Error :( : " + e.getMessage());
+            e.printStackTrace();
+        }
 
 
     }
