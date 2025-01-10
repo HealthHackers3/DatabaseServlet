876b15aacf71a6cf6bb99d9bce79532952224e46
Sam Clifford <sc2522@ic.ac.uk>
2025-01-10 22:24:19 +0000

    Servlet V5.6 image folder debug

--- src/main/java/api/img/postImgMap.java
+++ src/main/java/api/img/postImgMap.java
@@ -88,14 +88,32 @@ class postPostImage implements apiCommandHandler {
         upload.setSizeMax(MAX_FILE_SIZE); // Set max file size
 
         // Define the upload path relative to your project folder
-//        String projectPath = System.getProperty("user.dir").replace("\\", "/");
-         String projectPath = "/var/www/uploads";
+        String projectPath = System.getProperty("user.dir").replace("\\", "/");
+
         String uploadPath = projectPath + File.separator + UPLOAD_DIRECTORY;
         centralisedLogger.log("projectPath: " + projectPath);
         centralisedLogger.log("Upload path: " + uploadPath);
         centralisedLogger.log("File seperator: " + File.separator);
         centralisedLogger.log("uploadDirectory: " + UPLOAD_DIRECTORY);
         File uploadDir = new File(uploadPath);
+
+        File[] files = uploadDir.listFiles();
+        if (files == null || files.length == 0) {
+            centralisedLogger.log("No files or directories found in: " + uploadPath);
+        }
+        else {
+
+            centralisedLogger.log("Contents of directory: " + uploadPath);
+
+            // Iterate through and log directories
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    centralisedLogger.log("Directory: " + file.getName());
+                } else if (file.isFile()) {
+                    centralisedLogger.log("File: " + file.getName());
+                }
+            }
+        }
         if (!uploadDir.exists()) {
             try {
                 centralisedLogger.log("Making a new file");
